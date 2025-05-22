202720ca71470687750cb71aac23a5a58720f33d
TanishqParab <130289493+TanishqParab@users.noreply.github.com>
2025-05-22 21:02:15 +0530

    Update app.py

--- blue-green-deployment/modules/ecs/scripts/app.py
+++ blue-green-deployment/modules/ecs/scripts/app.py
@@ -15,7 +15,7 @@ def home():
             </head>
             <body>
                 <h1>Hello, Blue-Green Deployment on ECS</h1>
-                <div class="version">V80</div>
+                <div class="version">V82</div>
             </body>
         </html>
     '''
