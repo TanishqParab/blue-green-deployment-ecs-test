d79116fb89d76281d73f604932a4546d77e42094
TanishqParab <130289493+TanishqParab@users.noreply.github.com>
2025-05-28 17:02:09 +0530

    Update app.py

--- blue-green-deployment/modules/ec2/scripts/app.py
+++ blue-green-deployment/modules/ec2/scripts/app.py
@@ -15,7 +15,7 @@ def home():
             </head>
             <body>
                 <h1>Hello, Blue-Green Deployment on EC2</h1>
-                <div class="version">V10</div>
+                <div class="version">V12</div>
             </body>
         </html>
     '''
