d824a92881909589278dc039e340b342d48ef729
TanishqParab <130289493+TanishqParab@users.noreply.github.com>
2025-06-20 12:41:05 +0530

    Update app_3.py

--- blue-green-deployment/modules/ec2/scripts/app_3.py
+++ blue-green-deployment/modules/ec2/scripts/app_3.py
@@ -10,7 +10,7 @@ blog_posts = [
     {
         "id": "1",
         "title": "Welcome to Blue-Green Deployment Blog",
-        "content": "This is a demonstration of a blog application running with blue-green deployment on AWS EC2.",
+        "content": "This is a demo #2 of a blog application running with blue-green deployment on AWS EC2.",
         "author": "Admin",
         "date": "2023-06-15"
     },
